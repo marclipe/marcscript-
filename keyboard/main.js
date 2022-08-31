e533b64657ca485f91098c29fba84322c9eabda5
MarcosF19 <marcos.lima52@fatec.sp.gov.br>
2022-08-30 21:10:24 -0300

    Event

--- keyboard/main.js
+++ keyboard/main.js
@@ -1,7 +1,23 @@
 //get all keys
+const keys = document.querySelectorAll(".key")
+console.log(keys)
 
 //play notes
+function playNote(event) {
+    //keycode
+    console.log(event.type)
+
+    //typed or pressed key 
+
+    //if key exists
+
+    //play audio
+}
 
 //click with mouse
+keys.forEach(function(key) {
+    key.addEventListener("click", playNote)
+})
 
 //keyboard type
+window.addEventListener("keydown", playNote)
